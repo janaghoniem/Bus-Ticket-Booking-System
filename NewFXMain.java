7ad51e5253b4463ce0fa8bfa51ce6e09997d0078
janaghoniem <121458456+janaghoniem@users.noreply.github.com>
2023-12-21 16:21:08 +0200

    Add files via upload

---
+++ NewFXMain.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package project.trial;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jana
+ */
+public class NewFXMain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        //LOGIN - SIGNUP SARA
+        
+        //ADMIN PAGE MARIAM
+        
+        //MANAGE USERS BUTTON PAGE MARIAM
+        
+        //MANAGE VEHICLES BUTTON PAGE JANA
+        
+        //MANAGE TRIPS BUTTON PAGE ROAA
+        
+        //MANAGE BOOKINGS BUTTON PAGE NOURAN
+        
+        //RECEPTIONIST PAGE MARIAM
+        
+        //MANAGE GUESTS BUTTON PAGE SARA
+
+        
+
+        
+//        Scene scene = new Scene(root, 300, 250);
+//        
+//        primaryStage.setTitle("Bus-Ticket Booking System");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
